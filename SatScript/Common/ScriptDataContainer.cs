77f916239aaa824c94c8df302653e7b5b9724995
GON <hutabasuzukiryu@live.jp>
2019-05-03 21:54:50 +0900

    add: ScriptDataContainer

---
+++ SatScript/Common/ScriptDataContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SatScript.Common
+{
+    /// <summary>
+    /// スクリプト用データ
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class ScriptDataContainer<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// データを取得設定する
+        /// </summary>
+        /// <param name="key">オブジェクト</param>
+        /// <returns>データ</returns>
+        public new TValue this[TKey key]
+        {
+            get => ContainsKey(key) ? base[key] : default;
+            set => base[key] = value;
+        }
+    }
+}
