d596f3860776ef280d024df955742b718b8e10bf
GON <hutabasuzukiryu@live.jp>
2019-05-28 01:39:55 +0900

    add: MapEventResource

---
+++ SatPlayer/Game/Object/MapEvent/MapEventResource.cs
@@ -0,0 +1,66 @@
+﻿using BaseComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatPlayer.Game.Object.MapEvent
+{
+    /// <summary>
+    /// マップイベント共通リソース
+    /// </summary>
+    public class MapEventResource
+    {
+        static MapEventResource s_instance;
+
+        /// <summary>
+        /// インスタンスを取得
+        /// </summary>
+        public static MapEventResource Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                    if (asd.Engine.Graphics != null)
+                        s_instance = new MapEventResource();
+                    else
+                        s_instance = null;
+                return s_instance;
+            }
+        }
+
+        /// <summary>
+        /// メッセージボックス
+        /// </summary>
+        public asd.Texture2D MessageBoxTexture { get; set; }
+
+        /// <summary>
+        /// ネームボックス
+        /// </summary>
+        public asd.Texture2D NameBoxTexture { get; set; }
+
+        /// <summary>
+        /// メッセージフォント
+        /// </summary>
+        public asd.Font MessageFont { get; set; }
+
+        /// <summary>
+        /// テキストフォント
+        /// </summary>
+        public asd.Font NameFont { get; set; }
+
+        private MapEventResource()
+        {
+            MessageBoxTexture = TextureManager.LoadTexture("Static/messageBox.png");
+            NameBoxTexture = TextureManager.LoadTexture("Static/nameBox.png");
+            MessageFont = asd.Engine.Graphics.CreateFont("Static/messageFont.aff");
+            if (MessageFont == null)
+                Logger.Warning("MessageFont doesn't exist.");
+            MessageFont = asd.Engine.Graphics.CreateFont("Static/nameFont.aff");
+            if (NameFont == null)
+                Logger.Warning("NameFont doesn't exist.");
+        }
+
+    }
+}
